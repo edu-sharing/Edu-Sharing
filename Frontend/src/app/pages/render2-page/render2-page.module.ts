e360d70f5b4b367da84b496567bda8ac5aebc4ad
Torsten Simon <simon@edu-sharing.net>
2024-06-10 13:40:35 +0200

    feat:prepare rs module

--- Frontend/src/app/pages/render2-page/render2-page.module.ts
+++ Frontend/src/app/pages/render2-page/render2-page.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { Render2PageRoutingModule } from './render2-page-routing.module';
 import { Render2PageComponent } from './render2-page.component';
+import { RenderingModule, RenderComponent } from 'ngx-rendering-service';
 
 /**
  * new module for (kotlin based) rendering backend
  */
 @NgModule({
     declarations: [Render2PageComponent],
-    imports: [SharedModule, Render2PageRoutingModule],
+    imports: [SharedModule, Render2PageRoutingModule, RenderingModule, RenderComponent],
 })
 export class Render2PageModule {}
