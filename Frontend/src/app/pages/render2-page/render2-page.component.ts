7e8c1e23b9916a980ad2f70a52b839ea14fb5820
Torsten Simon <simon@edu-sharing.net>
2024-06-11 14:46:26 +0200

    feat:prepare rendering 2

--- Frontend/src/app/pages/render2-page/render2-page.component.ts
+++ Frontend/src/app/pages/render2-page/render2-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { OptionsHelperDataService, UIAnimation } from 'ngx-edu-sharing-ui';
 import { EventListener } from '../../core-module/core.module';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { combineLatest, forkJoin } from 'rxjs';
 import { HOME_REPOSITORY, NodeService, Node } from 'ngx-edu-sharing-api';
 import { RenderDataRequest } from 'ngx-rendering-service-api';
 
@@ -17,14 +17,16 @@ export class Render2PageComponent {
     node = signal<Node>(null);
     dummyRequest = signal<RenderDataRequest>(null);
     constructor(private route: ActivatedRoute, private nodeApi: NodeService) {
-        forkJoin([this.route.params, this.route.queryParams]).subscribe(([params, queryParams]) => {
-            this.nodeApi
-                .getNode(params.nodeId, {
-                    repository: queryParams.repo || HOME_REPOSITORY,
-                })
-                .subscribe((n) => {
-                    this.node.set(n);
-                    this.dummyRequest.set({
+        combineLatest([this.route.params, this.route.queryParams]).subscribe(
+            ([params, queryParams]) => {
+                this.nodeApi
+                    .getNode(params.node, {
+                        repository: queryParams.repo || HOME_REPOSITORY,
+                    })
+                    .subscribe((n) => {
+                        console.log(n);
+                        this.node.set(n);
+                        /*this.dummyRequest.set({
                         nodeId: n.ref.id,
                         hash: Math.random() + '',
                         size: parseInt(n.size),
@@ -32,8 +34,18 @@ export class Render2PageComponent {
                         type: 'image',
                         version: n.content.version,
                         mimeType: n.mimetype,
+                    });*/
+                        this.dummyRequest.set({
+                            nodeId: 'TEST_lviv.jpg',
+                            size: -1,
+                            type: 'file-image',
+                            hash: '1',
+                            mimeType: 'image/jpeg',
+                            version: '1.0',
+                            repoId: '',
+                        });
                     });
-                });
-        });
+            },
+        );
     }
 }
--- Frontend/src/app/pages/render2-page/render2-page.module.ts
+++ Frontend/src/app/pages/render2-page/render2-page.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
 import { Render2PageRoutingModule } from './render2-page-routing.module';
 import { Render2PageComponent } from './render2-page.component';
 import { RenderingModule, RenderComponent } from 'ngx-rendering-service';
+import { RenderingServiceApiModule } from 'ngx-rendering-service-api';
+import { environment } from '../../../environments/environment';
 
 /**
  * new module for (kotlin based) rendering backend
  */
 @NgModule({
     declarations: [Render2PageComponent],
-    imports: [SharedModule, Render2PageRoutingModule, RenderingModule, RenderComponent],
+    imports: [
+        Render2PageRoutingModule,
+        RenderingModule,
+        RenderComponent,
+        RenderingServiceApiModule.forRoot({
+            //rootUrl: environment.rsUrl
+        }),
+    ],
 })
 export class Render2PageModule {}
