d6549afd93b71939dbed32296beba58a23675447
Torsten Simon <simon@edu-sharing.net>
2024-06-14 11:38:21 +0200

    integrate different modules

--- Frontend/src/app/pages/render2-page/render2-page.component.ts
+++ Frontend/src/app/pages/render2-page/render2-page.component.ts
@@ -17,6 +17,52 @@ export class Render2PageComponent {
     node = signal<Node>(null);
     dummyRequest = signal<RenderDataRequest>(null);
     constructor(private route: ActivatedRoute, private nodeApi: NodeService) {
+        this.dummyRequest.set({
+            nodeId: 'TEST_lviv.jpg',
+            size: -1,
+            type: 'file-image',
+            hash: '1',
+            mimeType: 'image/jpeg',
+            version: '1.0',
+            repoId: '',
+        });
+        this.dummyRequest.set({
+            nodeId: 'TEST_4k.mp4',
+            size: -1,
+            type: 'file-video',
+            hash: '' + Math.random(),
+            mimeType: 'video/mpeg',
+            version: '1.0',
+            repoId: '',
+        });
+        this.dummyRequest.set({
+            nodeId: 'TEST_portrait.pdf',
+            size: -1,
+            type: 'file-pdf',
+            hash: '' + Math.random(),
+            mimeType: 'application/pdf',
+            version: '1.0',
+            repoId: '',
+        });
+        this.dummyRequest.set({
+            nodeId: 'TEST_lorem_ipsum.odt',
+            size: -1,
+            type: 'file-word',
+            hash: '' + Math.random(),
+            mimeType: 'application/vnd.oasis.opendocument.text',
+            version: '1.0',
+            repoId: '',
+        });
+        // url module
+        /*this.dummyRequest.set(undefined);
+        this.node.set({
+                title: "Test-Node",
+                mediatype: "link",
+                properties: {
+                    "ccm:wwwurl": ["https://www.youtube.com/watch?v=SVOuYquXuuc"]
+                }
+            } as unknown as Node);*/
+        /*
         combineLatest([this.route.params, this.route.queryParams]).subscribe(
             ([params, queryParams]) => {
                 this.nodeApi
@@ -26,15 +72,6 @@ export class Render2PageComponent {
                     .subscribe((n) => {
                         console.log(n);
                         this.node.set(n);
-                        /*this.dummyRequest.set({
-                        nodeId: n.ref.id,
-                        hash: Math.random() + '',
-                        size: parseInt(n.size),
-                        repoId: n.ref.repo,
-                        type: 'image',
-                        version: n.content.version,
-                        mimeType: n.mimetype,
-                    });*/
                         this.dummyRequest.set({
                             nodeId: 'TEST_lviv.jpg',
                             size: -1,
@@ -44,17 +81,10 @@ export class Render2PageComponent {
                             version: '1.0',
                             repoId: '',
                         });
-                        this.dummyRequest.set({
-                            nodeId: 'TEST_4k.mp4',
-                            size: -1,
-                            type: 'file-video',
-                            hash: '' + Math.random(),
-                            mimeType: 'video/mpeg',
-                            version: '1.0',
-                            repoId: '',
-                        });
+
                     });
             },
         );
+        */
     }
 }
