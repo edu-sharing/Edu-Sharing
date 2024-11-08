87df26f0f70971293b17ed077a7ff7520ef231fd
Sebastian Wollner <wollner@edu-sharing.net>
2024-11-08 14:40:22 +0100

    fix: NullPointerException on create person

--- Backend/services/core/src/main/java/org/edu_sharing/restservices/PersonDao.java
+++ Backend/services/core/src/main/java/org/edu_sharing/restservices/PersonDao.java
@@ -217,7 +217,7 @@ public class PersonDao {
 		newUserInfo.put(CCConstants.PROP_USER_LASTNAME, profile.getLastName());
 		newUserInfo.put(CCConstants.PROP_USER_EMAIL, profile.getEmail());
         newUserInfo.put(CCConstants.CM_PROP_PERSON_ABOUT, profile.getAbout());
-        newUserInfo.put(CCConstants.CM_PROP_PERSON_SKILLS, new ArrayList(Arrays.asList(profile.getSkills())));
+		newUserInfo.put(CCConstants.CM_PROP_PERSON_SKILLS, profile.getSkills() != null ? new ArrayList<>(Arrays.asList(profile.getSkills())) : null);
         newUserInfo.put(CCConstants.CM_PROP_PERSON_VCARD, profile.getVCard());
 		if(AuthorityServiceFactory.getLocalService().isGlobalAdmin()) {
 			newUserInfo.put(CCConstants.CM_PROP_PERSON_EDU_SCHOOL_PRIMARY_AFFILIATION, profile.getPrimaryAffiliation());
