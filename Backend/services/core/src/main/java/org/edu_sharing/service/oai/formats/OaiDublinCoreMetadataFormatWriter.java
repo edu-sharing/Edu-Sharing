7357a9af036f8b42908d696193e5c324e5860305
Sebastian Wollner <wollner@edu-sharing.net>
2024-11-15 13:18:16 +0100

    feat: oai dublin core use inverted naming

--- Backend/services/core/src/main/java/org/edu_sharing/service/oai/formats/OaiDublinCoreMetadataFormatWriter.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/oai/formats/OaiDublinCoreMetadataFormatWriter.java
@@ -19,6 +19,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWriter {
@@ -39,38 +40,38 @@ public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWri
 
         context.createAndAppendElement("dc:identifier", root, URLHelper.getNgRenderNodeUrl(context.getNodeId(), null));
         String handleId = propertyMapper.getString(CCConstants.CCM_PROP_PUBLISHED_HANDLE_ID);
-        if(StringUtils.isNotBlank(handleId)) {
+        if (StringUtils.isNotBlank(handleId)) {
             context.createAndAppendElement("dc:identifier", root, "info:eu-repo/semantics/altIdentifier/hdl/" + handleId);
         }
-        context.createAndAppendElement("dc:identifier", root, "UID:urn:uuid:"+propertyMapper.getString(CCConstants.SYS_PROP_NODE_UID));
+        context.createAndAppendElement("dc:identifier", root, "UID:urn:uuid:" + propertyMapper.getString(CCConstants.SYS_PROP_NODE_UID));
 
-        context.createAndAppendElement("dc:title", root, propertyMapper.getString(CCConstants.LOM_PROP_GENERAL_TITLE,  propertyMapper.getString(CCConstants.CM_NAME)));
+        context.createAndAppendElement("dc:title", root, propertyMapper.getString(CCConstants.LOM_PROP_GENERAL_TITLE, propertyMapper.getString(CCConstants.CM_NAME)));
         context.createAndAppendElement("dc:description", root, propertyMapper.getString(CCConstants.LOM_PROP_GENERAL_DESCRIPTION));
         context.createAndAppendElement("dc:language", root, propertyMapper.getString(CCConstants.LOM_PROP_GENERAL_LANGUAGE));
         context.createAndAppendElement("dc:subject", root, propertyMapper.getStringList(CCConstants.LOM_PROP_GENERAL_KEYWORD));
-        context.createAndAppendElement("dc:subject", root, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_CLASSIFICATION_KEYWORD).stream().map(x->String.format("https://d-nb.info/gnd/%s-%s", x.substring(1, x.length()-1), x.substring(x.length()-1))).collect(Collectors.toList()));
+        context.createAndAppendElement("dc:subject", root, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_CLASSIFICATION_KEYWORD).stream().map(x -> String.format("https://d-nb.info/gnd/%s-%s", x.substring(1, x.length() - 1), x.substring(x.length() - 1))).collect(Collectors.toList()));
         context.createAndAppendElement("dc:subject", root, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_TAXON_ID));
 
         context.createAndAppendElement("dc:creator", root, propertyMapper.getString(CCConstants.CCM_PROP_IO_UNIVERSITY));
-        context.createAndAppendElement("dc:creator", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_METADATACONTRIBUTER_CREATOR)));
-        context.createAndAppendElement("dc:publisher", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_PUBLISHER)));
+        context.createAndAppendElement("dc:creator", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_METADATACONTRIBUTER_CREATOR), null));
+        context.createAndAppendElement("dc:publisher", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_PUBLISHER), null));
 
         context.createAndAppendElement("dc:date", root, handleDate(context, CCConstants.CM_PROP_C_CREATED));
         context.createAndAppendElement("dc:date", root, handleDate(context, CCConstants.CCM_PROP_IO_PUBLISHED_DATE));
 
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_AUTHOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_CONTENT_PROVIDER)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_EDITOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_EDUCATIONAL_VALIDATOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_GRAPHICAL_DESIGNER)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_INITIATOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_INSTRUCTIONAL_DESIGNER)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_SCRIPT_WRITER)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_SUBJECT_MATTER_EXPERT)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TECHNICAL_IMPLEMENTER)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TECHNICAL_VALIDATOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TERMINATOR)));
-        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_VALIDATOR)));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_AUTHOR), "Author"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_CONTENT_PROVIDER), "Content Provider"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_EDITOR), "Editor"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_EDUCATIONAL_VALIDATOR),  "Educational Validator"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_GRAPHICAL_DESIGNER), "Graphical Designer"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_INITIATOR), "Initiator"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_INSTRUCTIONAL_DESIGNER),  "Instructional Designer"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_SCRIPT_WRITER), "Publisher"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_SUBJECT_MATTER_EXPERT),  "Script Writer"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TECHNICAL_IMPLEMENTER),  "Technical Implementer"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TECHNICAL_VALIDATOR),  "Technical Validator"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_TERMINATOR), "Terminator"));
+        context.createAndAppendElement("dc:contributor", root, prepareContributor(context, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_VALIDATOR), "Validator"));
 
         context.createAndAppendElement("dc:format", root, propertyMapper.getString(CCConstants.LOM_PROP_TECHNICAL_FORMAT));
         context.createAndAppendElement("dc:source", root, propertyMapper.getString(CCConstants.CCM_PROP_IO_REPLICATIONSOURCE));
@@ -79,24 +80,23 @@ public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWri
 
         Integer tarFrom = propertyMapper.getInteger(CCConstants.CCM_PROP_IO_REPL_EDUCATIONAL_TYPICALAGERANGEFROM);
         Integer tarTo = propertyMapper.getInteger(CCConstants.CCM_PROP_IO_REPL_EDUCATIONAL_TYPICALAGERANGETO);
-        if(tarFrom != null && tarTo != null) {
+        if (tarFrom != null && tarTo != null) {
             context.createAndAppendElement("dc:audience", root, String.format("Learner (%s-%s)", tarFrom, tarTo));
         }
         context.createAndAppendElement("dc:audience", root, propertyMapper.getStringList(CCConstants.CCM_PROP_IO_REPL_EDUCATIONAL_INTENDEDENDUSERROLE));
 
 
-
         String commonLicenceKey = propertyMapper.getString(CCConstants.CCM_PROP_IO_COMMONLICENSE_KEY);
-        if(StringUtils.isBlank(commonLicenceKey)) {
+        if (StringUtils.isBlank(commonLicenceKey)) {
             context.createAndAppendElement("dc:rights", root, propertyMapper.getBoolean(CCConstants.LOM_PROP_RIGHTS_COPY_RIGHT) ? "info:eu-repo/semantics/restrictedAccess" : "info:eu-repo/semantics/openAccess");
             context.createAndAppendElement("dc:rights", root, propertyMapper.getBoolean(CCConstants.LOM_PROP_RIGHTS_RIGHTS_DESCRIPTION));
-        }else {
+        } else {
             String commonLicenseLocale = propertyMapper.getString(CCConstants.CCM_PROP_IO_COMMONLICENSE_CC_LOCALE, Locale.getDefault().getLanguage());
             String commonLicenseVersion = propertyMapper.getString(CCConstants.CCM_PROP_IO_COMMONLICENSE_CC_VERSION);
             String url = new LicenseService().getLicenseUrl(commonLicenceKey, commonLicenseLocale, commonLicenseVersion);
 
             boolean isOpenAccess = commonLicenseLocale.equals("CC_0") || commonLicenseLocale.equals("PDM");
-            context.createAndAppendElement("dc:rights", root, isOpenAccess ? "info:eu-repo/semantics/openAccess":"info:eu-repo/semantics/restrictedAccess");
+            context.createAndAppendElement("dc:rights", root, isOpenAccess ? "info:eu-repo/semantics/openAccess" : "info:eu-repo/semantics/restrictedAccess");
             context.createAndAppendElement("dc:rights", root, url);
         }
     }
@@ -109,13 +109,17 @@ public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWri
         element.setAttribute("xsi:schemaLocation", "http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd");
     }
 
-    protected List<String> prepareContributor(Context context, List<String> contrib) {
-        return contrib.stream()
+    protected List<String> prepareContributor(Context context, List<String> contrib, String role) {
+        Stream<String> stream = contrib.stream()
                 //sometimes there are empty values in list
                 .filter(StringUtils::isNotBlank)
-//                .map(VCardConverter::getNameForVCardString)
-                .map(x->cleanupVCardEMail(context,x))
-                .collect(Collectors.toList());
+                .map(VCardConverter::getInvertedNameForVCardString);
+
+        if(StringUtils.isNotBlank(role)){
+            stream = stream.map(x->String.format("%s (%s)", x, role));
+        }
+
+        return stream.collect(Collectors.toList());
     }
 
     private String cleanupVCardEMail(Context context, String vCard) {
@@ -130,7 +134,7 @@ public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWri
         return VCardConverter.removeEMails(vCard);
     }
 
-    private String handleDate(Context context, String propertyId){
+    private String handleDate(Context context, String propertyId) {
         PropertyMapper propertyMapper = context.getPropertyMapper();
         String result = null;
         try {
@@ -142,7 +146,7 @@ public class OaiDublinCoreMetadataFormatWriter extends AbstractMetadataFormatWri
             }
         } catch (Exception e) {
             Long date = propertyMapper.getLong(propertyId);
-            if(date != null) {
+            if (date != null) {
                 result = ISO8601DateFormat.format(new Date(date));
             }
         }
