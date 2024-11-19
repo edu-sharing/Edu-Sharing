a6455445719f331a7963341557d2a9c933330866
Torsten Simon <simon@edu-sharing.net>
2024-11-19 14:28:46 +0100

    fix:rollback spinner

--- Frontend/projects/edu-sharing-ui/src/lib/spinner/spinner.component.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/spinner/spinner.component.ts
@@ -1,12 +1,12 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'es-spinner',
     templateUrl: 'spinner.component.html',
     styleUrls: ['spinner.component.scss'],
     standalone: true,
-    imports: [MatProgressSpinnerModule],
+    imports: [TranslateModule],
 })
 export class SpinnerComponent {
     @HostBinding('attr.data-test') readonly dataTest = 'loading-spinner';
