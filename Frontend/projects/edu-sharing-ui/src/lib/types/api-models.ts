b37b45431abe124d159d986c8e90f57f441f163b
Torsten Simon <simon@edu-sharing.net>
2025-01-07 17:36:10 +0100

    fix:focus mds input fix:observe connector node

--- Frontend/projects/edu-sharing-ui/src/lib/types/api-models.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/types/api-models.ts
@@ -1,4 +1,8 @@
 import { Node } from 'ngx-edu-sharing-api';
 export interface VirtualNode extends Node {
     virtual: boolean;
+    /**
+     * observe this node for changes (i.e. connector nodes)
+     */
+    observe?: boolean;
 }
