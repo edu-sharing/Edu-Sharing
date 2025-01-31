3f85bccbd25bc5a31d6c60b200bd23aa398005f6
ziegler <ziegler@edu-sharing.net>
2025-01-31 11:19:57 +0100

    feat: update render request

--- Frontend/src/app/pages/render2-page/render-wrapper-component/render-wrapper.component.ts
+++ Frontend/src/app/pages/render2-page/render-wrapper-component/render-wrapper.component.ts
@@ -146,6 +146,8 @@ export class RenderWrapperComponent implements OnChanges {
                 type: this.node().mediatype,
                 repoId: this.node().ref.repo,
                 version: this.node().content.version,
+                resourceType:
+                    this.node().properties[RestConstants.CCM_PROP_CCRESSOURCETYPE][0] ?? '',
                 // the replication source flag can be set in order to trigger special treatments
                 // in the backend. For example, it can be used for sodix paid media in order to
                 // fetch two instead of one url. This logic has to be implemented
